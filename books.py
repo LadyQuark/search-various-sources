4bbc9e948a8e934bcaea144a6e92202c14463818
Dipika <dips@Dipikas-iMac.local>
2022-08-24 21:39:18 +0530

    Passing params via argument

--- books.py
+++ books.py
@@ -1,7 +1,5 @@
-import json
 import os
 import requests
-import urllib.parse
 from dotenv import load_dotenv, find_dotenv
 
 # Get API key from .env
@@ -10,12 +8,16 @@ API_KEY = os.getenv('GOOGLEBOOKS_API_KEY')
      
 
 def search_googlebooks(search_term, limit=10):
-    query = urllib.parse.quote(search_term)
-    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={API_KEY}&maxResults={limit}"
+    payload = {
+        "q": requests.utils.quote(search_term),
+        "key": API_KEY,
+        "maxResults": limit,
+    }
+    url = "https://www.googleapis.com/books/v1/volumes"
 
     # Get response
     try:
-        response = requests.get(url)
+        response = requests.get(url, params=payload)
         response.raise_for_status()
     except requests.RequestException as e:
         print(f"Unable to search Google Books for {search_term}: {e}")

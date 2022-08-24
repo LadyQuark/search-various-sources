447711f33f4656f634cb244ae4abeb6eef3a06f5
Dipika <dips@Dipikas-iMac.local>
2022-08-24 11:27:56 +0530

    Searches YouTube using YouTube Data API v3 GET requests and returns a list of dicts: title, url

---
+++ videos.py
@@ -0,0 +1,33 @@
+import os
+import requests
+import urllib.parse
+from dotenv import load_dotenv, find_dotenv
+
+# Get API key from .env
+load_dotenv(find_dotenv())
+API_KEY = os.getenv('YOUTUBE_API_KEY')
+     
+def search_youtube(search_term, limit=10, country="US", lang="en"):
+    # Construct request URL
+    query = urllib.parse.quote(search_term)
+    params = f"part=snippet&type=video&q={query}&key={API_KEY}&maxResults={limit}"
+    params += f"&regionCode={country}&relevanceLanguage{lang}"
+    url = f"https://www.googleapis.com/youtube/v3/search?{params}"
+    # Get response
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Unable to search YouTube for {search_term}: {e}")
+        return []
+
+    data = response.json()
+
+    items = data['items']
+    results = []
+    for item in items:
+        results.append({
+            'title': item['snippet']['title'],
+            'url': f"https://www.youtube.com/watch?v={item['id']['videoId']}",
+        })   
+    return results

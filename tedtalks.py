fe1c352e86551be4e55fbc1a24a92356b69ee0fa
Dipika Bothra <dips@Dipikas-iMac.local>
2022-09-06 21:57:47 +0530

    Removed function that searches RapidAPI. ted_youtube_search_and_transform() searches youtube channels and returns transformed items

--- tedtalks.py
+++ tedtalks.py
@@ -1,39 +1,46 @@
-import requests
-import os
-from dotenv import load_dotenv, find_dotenv
+from videos import search_youtube_channel
+from transform_for_db import transform_youtube_item
+import logging
+from common import load_existing_json_file
 
-# Get API key from .env
-load_dotenv(find_dotenv())
-API_KEY = os.getenv('RAPIDAPI_KEY')
+logger = logging.getLogger('ted-log')
 
-def search_ted(search_term, limit=10):
-    """ 
-    Searches TED for given search term using API from RapidAPI 
-    https://rapidapi.com/Yakup7/api/ted-talk-api/ 
-    and outputs list of title and url, default count of 10
-    """
+CHANNEL_TED = "UCAuUUnT6oDeKwE6v1NGQxug"
+CHANNEL_TEDED = "UCsooa4yRKGN_zEE8iknghZA"
+TED_DB = load_existing_json_file(folder="db", name="ted_db")
+
+
+def ted_youtube_search_and_transform(search_term, limit=10, include_ted_ed=False):
+    # Initialise variables
+    search_results = []
+    db_items = []
     
-    # Construct URL
-    url = "https://ted-talk-api.p.rapidapi.com/talks"
-    payload = {
-        "keyword": search_term,
-    }
-    headers = {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "ted-talk-api.p.rapidapi.com"
-    }
-    payload_str = parse.urlencode(payload, quote_via=parse.quote)
-    # Get Response
-    try:
-        response = requests.get(url, headers=headers, params=payload_str)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        print(f"Unable to search Ted for {search_term}: {e}")
-        return []
-    # Parse response
-    data = response.json()
-    results = [item for item in data]
-    print(f"Results: {len(results)}")
-    print(response.url)
+    # Search for videos in TED's channels on Youtube
+    if include_ted_ed:
+        limit = limit // 2
+    ted = search_youtube_channel(search_term, CHANNEL_TED, limit)
+    search_results.extend(ted)
+    if include_ted_ed:
+        ted_ed = search_youtube_channel(search_term, CHANNEL_TEDED, limit)
+        search_results.extend(ted_ed)
+    
+    # Transform each result
+    for result in search_results:
+        try:
+            item = transform_youtube_item(result, search_term, type="tedtalks")
+            youtube_url = item['metadata']['url']
+            ted_item = TED_DB[youtube_url]
+            item['title'] = ted_item['title']
+            item['description'] = ted_item['description']
+            item['authors'] = ted_item['speaker']
+            item['metadata']['url'] = ted_item['url']
+            item['metadata']['video_length'] = ted_item['length']
+            item['publishedDate'] = ted_item['publishdate']
+            item['original'].append(ted_item)
 
-    return results[:limit]
+        except Exception as e:
+            logger.warning(f"Transform error: {e}")
+        else:
+            db_items.append(item)
+    
+    return db_items

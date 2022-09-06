bd24098ca7306fa6caee6efa0801a5e1e2c1b3dd
Dipika Bothra <dips@Dipikas-iMac.local>
2022-09-06 21:49:37 +0530

    Added comments

--- tedtalks.py
+++ tedtalks.py
@@ -8,7 +8,7 @@ API_KEY = os.getenv('RAPIDAPI_KEY')
 
 def search_ted(search_term, limit=10):
     """ 
-    Searches Scopus for given search term using API from RapidAPI 
+    Searches TED for given search term using API from RapidAPI 
     https://rapidapi.com/Yakup7/api/ted-talk-api/ 
     and outputs list of title and url, default count of 10
     """
@@ -16,26 +16,24 @@ def search_ted(search_term, limit=10):
     # Construct URL
     url = "https://ted-talk-api.p.rapidapi.com/talks"
     payload = {
-        "keyword": requests.utils.quote(search_term),
+        "keyword": search_term,
     }
     headers = {
         "X-RapidAPI-Key": API_KEY,
         "X-RapidAPI-Host": "ted-talk-api.p.rapidapi.com"
     }
+    payload_str = parse.urlencode(payload, quote_via=parse.quote)
     # Get Response
     try:
-        response = requests.get(url, headers=headers, params=payload)
+        response = requests.get(url, headers=headers, params=payload_str)
         response.raise_for_status()
     except requests.RequestException as e:
         print(f"Unable to search Ted for {search_term}: {e}")
         return []
     # Parse response
     data = response.json()
-    results = []
-    for item in data:
-        results.append({
-            'title': item['title'],
-            'url': item['link']
-        })
+    results = [item for item in data]
+    print(f"Results: {len(results)}")
+    print(response.url)
 
     return results[:limit]

a71f3ca48a8e3fbedb255e55eace7c34ded89442
Dipika Bothra <dips@Dipikas-iMac.local>
2022-12-02 10:58:43 +0530

    research_search_and_transform now uses new get_sciencedirect() instead of get_abstract(). search_scopus() now only returns results that contain  value

--- research.py
+++ research.py
@@ -3,7 +3,7 @@ import logging
 import requests
 from urllib import parse
 from dotenv import load_dotenv, find_dotenv
-from transform_for_db import transform_scopus
+from transform_for_db import transform_scopus, transform_scd
 from sys import exit
 from progress import progress
 import pprint
@@ -66,11 +66,12 @@ def search_scopus(search_term, limit=10):
     scopus_results = get_scopus(payload)
     for i, result in enumerate(scopus_results):
         progress(i+1, limit)
-        results.extend(result)
+        results.extend([item for item in result if 'pii' in item])
+        
         if len(results) >= limit:
             scopus_results.close()
 
-    return results[:limit]  
+    return results[:limit]
 
 
 def research_search_and_transform(search_term, limit=10):
@@ -78,28 +79,32 @@ def research_search_and_transform(search_term, limit=10):
     Search Scopus and return transformed results
     """
     search_results = search_scopus(search_term, limit)
-    pp.pprint(search_results)
     
     total = len(search_results)
     db_items = []
     
     for i, result in enumerate(search_results):
         progress(i+1, total)
+
+        pii = result['pii']
+        article = get_sciencedirect(pii)
+        if not article:
+            continue
+        item = transform_scd(article)
+        if not item:
+            continue
+        db_items.append(item)
         # Transform result
-        try:
-            item = transform_scopus(result, search_term)
-        except Exception as e:
-            logger.warning(f"Transform error: {e}")
-            continue
+        # item = transform_scopus(result, search_term)
+        # if not item: 
+        #     continue
         
-        # Get abstract and append if available
-        if not 'prism:doi' in item['original'][0]:
-            continue
-        doi = item['original'][0]['prism:doi']
-        description = get_abstract(doi)
-        if description:
-            item['description'] = description.strip()
-            db_items.append(item)
+        # # Get abstract and append if available
+        # doi = item['original'][0]['prism:doi']
+        # description = get_abstract(doi)
+        # if description:
+        #     item['description'] = description.strip()
+        #     db_items.append(item)
 
     return db_items
 
@@ -141,3 +146,32 @@ def get_abstract(doi):
     else:
         return abstract    
 
+def get_sciencedirect(pii):
+
+    # Construct request URL
+    API_url = "https://api.elsevier.com/content/article/pii/" + pii
+    payload = {
+        "apiKey": API_KEY,
+        "httpAccept": "application/json",        
+    }
+    # Get response
+    try:
+        response = requests.get(API_url, params=payload)
+        response.raise_for_status()
+    # Handle errors
+    except requests.RequestException as e:
+        if response.status_code == 429:
+            raise Exception("Elsevier API Quota exceeded")
+        elif response.status_code == 400:
+            raise Exception(f"Invalid PII/publication ID {id}")
+        elif response.status_code == 404:
+            raise Exception(f"Resource not found for {id}")
+        else:
+            raise Exception(f"Unable to fetch article {id}: {e}")
+    # Get JSON response
+    data = response.json()
+    # Check key
+    if 'full-text-retrieval-response' not in data:
+        raise Exception(f"Unable to parse article {id}")
+    
+    return data['full-text-retrieval-response']

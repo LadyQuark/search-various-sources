a324098a6892a9780d5e30fabae5944eda378325
Dipika Bothra <dips@Dipikas-iMac.local>
2023-06-14 19:30:13 +0530

    Added argument to add delay to searching podcasts

--- search_save_mongo.py
+++ search_save_mongo.py
@@ -12,6 +12,7 @@ from podcasts import podcast_eps_search_and_transform
 from tedtalks import ted_youtube_search_and_transform
 from videos import youtube_search_and_transform
 from research import research_search_and_transform
+from time import sleep
 
 load_dotenv(find_dotenv())
 pp = pprint.PrettyPrinter(depth=6)  
@@ -26,7 +27,7 @@ collection = db['knowledgeitem_master']
 
 TOTAL_RESULTS = 100
 
-def search_various_sources(search_list, limit=TOTAL_RESULTS):
+def search_various_sources(search_list, limit=TOTAL_RESULTS, delay=False):
     
     # Dict containing results of each category
     results = {
@@ -51,7 +52,11 @@ def search_various_sources(search_list, limit=TOTAL_RESULTS):
         for i, search_term in enumerate(search_list):
             progress(i+1, total, type)
             # Get results
-            search_results = fn(search_term, limit)
+            if type == "podcasts":
+                search_results = fn(search_term, limit)
+                if delay: sleep(5)
+            else:
+                search_results = fn(search_term, limit)
             # Add results to results list
             if search_results:
                 results[type].extend(search_results[:limit])
@@ -63,6 +68,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("search_file", help="Path of the text file containing search terms")
     parser.add_argument("-l", "--limit", help="Total results", type=int, default=TOTAL_RESULTS)
+    parser.add_argument("-d", "--delay", help="Delay searches because of rate limits", action="store_true")
     args = parser.parse_args()
     
     # Get search terms from text file at `args.search_time`
@@ -71,7 +77,7 @@ def main():
     except Exception as e:
         exit(e)
 
-    results = search_various_sources(search_list=search_list, limit=args.limit)
+    results = search_various_sources(search_list=search_list, limit=args.limit, delay=args.delay)
     
     print("Inserting in MongoDB")
     for type in results:
